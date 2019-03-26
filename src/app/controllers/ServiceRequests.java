0dadb4486c2274ede0cb1663947c929b0265a208
Connor <cjburri@wpi.edu>
2019-03-26 18:40:01 -0400

    Skeleton for the service request feature controller

---
+++ src/app/controllers/ServiceRequests.java
@@ -0,0 +1,57 @@
+/**
+ * Sample Skeleton for 'serviceRequests.fxml' Controller Class
+ */
+
+package app.Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+
+public class ServiceRequests {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="logoutButton"
+    private Button logoutButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="roomNumber"
+    private TextArea roomNumber; // Value injected by FXMLLoader
+
+    @FXML // fx:id="languageSelection"
+    private ChoiceBox<?> languageSelection; // Value injected by FXMLLoader
+
+    @FXML // fx:id="requestButton"
+    private Button requestButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="notesText"
+    private TextArea notesText; // Value injected by FXMLLoader
+
+    @FXML
+    void goToIdle(ActionEvent event) {
+
+    }
+
+    @FXML
+    void makeRequest(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert logoutButton != null : "fx:id=\"logoutButton\" was not injected: check your FXML file 'serviceRequests.fxml'.";
+        assert roomNumber != null : "fx:id=\"roomNumber\" was not injected: check your FXML file 'serviceRequests.fxml'.";
+        assert languageSelection != null : "fx:id=\"languageSelection\" was not injected: check your FXML file 'serviceRequests.fxml'.";
+        assert requestButton != null : "fx:id=\"requestButton\" was not injected: check your FXML file 'serviceRequests.fxml'.";
+        assert notesText != null : "fx:id=\"notesText\" was not injected: check your FXML file 'serviceRequests.fxml'.";
+
+    }
+}
