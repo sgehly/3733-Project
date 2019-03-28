e761a8644839d7476a0cf20c0580f406185b7e3a
Ken Desrosiers <kdesrosiers@wpi.edu>
2019-03-28 12:23:07 -0400

    Simpler A*

--- src/app/AStar.java
+++ src/app/AStar.java
@@ -63,50 +63,53 @@ public class AStar {
 
     //Actual A* algorithm
     public List<Node> findPath(Node start, Node end) {
-        double cost = 0;
         start.setG(0);
         start.setF(0);
+        List<Node> path = new ArrayList<>(); //where the final path will be stored
         PriorityQueue openList = new PriorityQueue(); //prio queue to keep track of lowest f score
         openList.addtoHeap(start);
-        HashMap<String, Node> closedList = new HashMap<String, Node>();
-        while (!openList.getHeap().get(0).getId().equals(end.getId())) { //while the node with the lowest f score isn't the goal
-            openList.buildHeap(); //build the heap again
-            Node current = openList.getHeap().get(0); //get the current lowest f score node
+        HashMap<String, Node> closedList = new HashMap<String, Node>(); //closed list
+        while(openList.getHeapSize() != 0){ //while the open list is not empty
+            openList.buildHeap(); //bring best node to head of open list
+            Node current = openList.getHeap().get(0); //set it to current
             openList.getHeap().remove(0); //remove it from the open list
-            closedList.put(current.getId(), current); //put it in the closed list
-            for (Edge e : current.getEdges()) { //for every single neighbor of that node
+            closedList.put(current.getId(), current); //add it to the closed list
+            if(current.getId().equals(end.getId())){ //if the goal is found, add nodes along path to the path list
+                current = end;
+                while(!current.getId().equals(start.getId())){ //while the current node isn't the starting node
+                    path.add(current); //add it to the final path
+                    current = current.getParent(); //go to the parent
+                }
+                path.add(start); //add the start to the path and return the path
+                return path;
+            }
+            for(Edge e : current.getEdges()){ //for every neighbor of the current node
                 Node neighbor = e.getEndNode();
-                cost = current.getG() + neighbor.getDistance(current);
-                if (openList.findNode(neighbor) && cost < neighbor.getG()) { //if the node is already on the open list, but its cost can be lowered
-                    for (Node n : openList.getHeap()) {
-                        if (n.getId().equals(neighbor.getId())) {
-                            int index = openList.getHeap().indexOf(n); //find it in the open list and record its index
-                            openList.getHeap().remove(index); //remove it from the open list
+                if(closedList.containsKey(neighbor.getId())){ //if its in the closed list, ignore it
+                    continue;
+                }
+                neighbor.setG(neighbor.getDistance(current) + current.getG()); //set its g, h, f, and parent
+                neighbor.setH(neighbor.getDistance(end));
+                neighbor.setF(neighbor.getG() + neighbor.getH());
+                neighbor.setParent(current);
+                for(Node n : openList.getHeap()){
+                    if(neighbor == n){ //if its in the open list
+                        if(neighbor.getG() >= n.getG()){ //if the cost of the node cannot be lowered, continue
+                            continue;
+                        }
+                        else{ //else if the cost can be lowered
+                            n.setParent(current);
+                            n.setG(n.getDistance(current) + current.getG());
+                            n.setF(n.getG() + n.getH());
                         }
                     }
                 }
-                if (closedList.containsKey(neighbor.getId()) && cost < neighbor.getG()) { //if its on the closed list and its cost can be lowered
-                    closedList.remove(neighbor.getId()); //remove it from the closed list
-                }
-                if (!openList.findNode(neighbor) && !closedList.containsKey(neighbor.getId())) { //if its not on either list
-                    if (neighbor.getFloor().equals(end.getFloor())) { //if it is on the same floor as the destination node
-                        neighbor.setG(cost); //set its g, h, f, and parent
-                        openList.addtoHeap(neighbor);
-                        neighbor.setH(neighbor.getDistance(end));
-                        neighbor.setF(neighbor.getG() + neighbor.getH());
-                        neighbor.setParent(current);
-                    }
+                if(neighbor.getFloor().equals(end.getFloor())) { //if the neighbor is on the same floor as the end node, add it the open list
+                    openList.addtoHeap(neighbor);
                 }
             }
         }
-        List<Node> path = new ArrayList<>(); //reconstruct and return the completed path
-        Node current = end;
-        while (!current.getId().equals(start.getId())) {
-            path.add(current);
-            current = current.getParent();
-        }
-        path.add(start);
-        return path;
+        return null;
     }
 
     /**
