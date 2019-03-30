79697468aa6bba5ee6c6d593631096d275b6890e
Connor <cjburri@wpi.edu>
2019-03-30 13:14:38 -0400

    updated controller and service requests functionality

--- src/app/controllers/ServiceRequests.java
+++ src/app/controllers/ServiceRequests.java
@@ -2,9 +2,6 @@ package app.controllers;
 
 import app.Main;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,28 +11,33 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 
+import java.awt.event.MouseEvent;
+
 public class ServiceRequests {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+    @FXML
+    private Button logoutButton;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+    @FXML
+    private TextArea sanitationRoomNumber;
 
-    @FXML // fx:id="logoutButton"
-    private Button logoutButton; // Value injected by FXMLLoader
+    @FXML
+    private Button sanitationRequestButton;
 
-    @FXML // fx:id="roomNumber"
-    private TextArea roomNumber; // Value injected by FXMLLoader
+    @FXML
+    private TextArea sanitationNotes;
 
-    @FXML // fx:id="languageSelection"
-    private ChoiceBox<?> languageSelection; // Value injected by FXMLLoader
+    @FXML
+    private TextArea languageRoomNumber;
 
-    @FXML // fx:id="requestButton"
-    private Button requestButton; // Value injected by FXMLLoader
+    @FXML
+    private ChoiceBox<?> languageSelection;
 
-    @FXML // fx:id="notesText"
-    private TextArea notesText; // Value injected by FXMLLoader
+    @FXML
+    private Button languageRequestButton;
+
+    @FXML
+    private TextArea languageNotes;
 
     @FXML
     public void logout() throws Exception {
@@ -51,17 +53,28 @@ public class ServiceRequests {
     }
 
     @FXML
-    void makeRequest(ActionEvent event) {
-        //to be implemented later on
+    void checkRoomValidity(MouseEvent event) {
+
+    }
+
+    @FXML
+    void makeSanitationRequest(ActionEvent event) {
+
+    }
+
+    @FXML
+    void makeLanguageRequest(ActionEvent event) {
+
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        //general asserts
         assert logoutButton != null : "fx:id=\"logoutButton\" was not injected: check your FXML file 'serviceRequests.fxml'.";
-        assert roomNumber != null : "fx:id=\"roomNumber\" was not injected: check your FXML file 'serviceRequests.fxml'.";
+        //language asserts
+        assert languageRoomNumber != null : "fx:id=\"roomNumber\" was not injected: check your FXML file 'serviceRequests.fxml'.";
         assert languageSelection != null : "fx:id=\"languageSelection\" was not injected: check your FXML file 'serviceRequests.fxml'.";
-        assert requestButton != null : "fx:id=\"requestButton\" was not injected: check your FXML file 'serviceRequests.fxml'.";
-        assert notesText != null : "fx:id=\"notesText\" was not injected: check your FXML file 'serviceRequests.fxml'.";
-
+        assert languageRequestButton != null : "fx:id=\"requestButton\" was not injected: check your FXML file 'serviceRequests.fxml'.";
+        assert languageNotes != null : "fx:id=\"notesText\" was not injected: check your FXML file 'serviceRequests.fxml'.";
     }
 }
