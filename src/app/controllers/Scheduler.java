7c9333d2278c7f0506d19851b12e5e53beddbd26
Phillip Abell <pbabell@wpi.edu>
2019-03-30 11:49:24 -0400

    Added scheduler page

---
+++ src/app/controllers/Scheduler.java
@@ -0,0 +1,48 @@
+
+package app.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import app.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+public class Scheduler {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="zoomLvl"
+    private Slider zoomLvl; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imageView"
+    private HBox imageView; // Value injected by FXMLLoader
+
+    @FXML // fx:id="image"
+    private ImageView image; // Value injected by FXMLLoader
+
+    @FXML
+    private void navigateToHome() throws Exception{
+        Parent pane = FXMLLoader.load(Main.getFXMLURL("home"));
+        Scene scene = new Scene(pane);
+        Main.getStage().setScene(scene);
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert zoomLvl != null : "fx:id=\"zoomLvl\" was not injected: check your FXML file 'scheduler.fxml'.";
+        assert imageView != null : "fx:id=\"imageView\" was not injected: check your FXML file 'scheduler.fxml'.";
+        assert image != null : "fx:id=\"image\" was not injected: check your FXML file 'scheduler.fxml'.";
+
+    }
+}
