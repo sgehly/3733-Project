e36c2f40e7cb7b917c43bb3c41aa3519e825ac39
Sam Gehly <sam@gehly.net>
2019-03-31 15:56:26 -0400

    Init image

--- src/app/controllers/Scheduler.java
+++ src/app/controllers/Scheduler.java
@@ -64,7 +64,7 @@ public class Scheduler {
 
         Image source = null;
         try {
-            String path = getClass().getResource("/resources/maps/01_thefirstfloor.png").toString().replace("file:","");
+            String path = getClass().getResource("/resources/maps/scheduler.jpg").toString().replace("file:","");
             System.out.println(path);
             source = new Image(new FileInputStream(path));
         } catch (FileNotFoundException e) {
@@ -92,92 +92,5 @@ public class Scheduler {
         System.out.println("height = " + height + "\nwidth = " + width);
         HBox zoom = new HBox(10);
         zoom.setAlignment(Pos.CENTER);
-
-        zoomLvl.setMax(10);
-        zoomLvl.setMin(1);
-
-        offSetX = width / 2;
-        offSetY = height / 2;
-
-        Slider Hscroll = new Slider();
-        Hscroll.setMin(0);
-        Hscroll.setMax(width);
-        Hscroll.setMaxWidth(image.getFitWidth());
-        Hscroll.setMinWidth(image.getFitWidth());
-        Hscroll.setTranslateY(20);
-        Slider Vscroll = new Slider();
-        Vscroll.setMin(0);
-        Vscroll.setMax(height);
-        Vscroll.setMaxHeight(image.getFitHeight());
-        Vscroll.setMinHeight(image.getFitHeight());
-        Vscroll.setOrientation(Orientation.VERTICAL);
-        Vscroll.setTranslateX(-20);
-
-        //imageView.setAlignment(Hscroll, Pos.CENTER);
-        //imageView.setAlignment(Vscroll, Pos.CENTER_LEFT);
-
-        Hscroll.valueProperty().addListener(e -> {
-            offSetX = Hscroll.getValue();
-            zoomlvl = zoomLvl.getValue();
-            double newValue = (double) ((int) (zoomlvl * 10)) / 10;
-            if (offSetX < (width / newValue) / 2) {
-                offSetX = (width / newValue) / 2;
-            }
-            if (offSetX > width - ((width / newValue) / 2)) {
-                offSetX = width - ((width / newValue) / 2);
-            }
-
-            image.setViewport(new Rectangle2D(offSetX - ((width / newValue) / 2), offSetY - ((height / newValue) / 2), width / newValue, height / newValue));
-        });
-        Vscroll.valueProperty().addListener(e -> {
-            offSetY = height - Vscroll.getValue();
-            zoomlvl = zoomLvl.getValue();
-            double newValue = (double) ((int) (zoomlvl * 10)) / 10;
-            if (offSetY < (height / newValue) / 2) {
-                offSetY = (height / newValue) / 2;
-            }
-            if (offSetY > height - ((height / newValue) / 2)) {
-                offSetY = height - ((height / newValue) / 2);
-            }
-            image.setViewport(new Rectangle2D(offSetX - ((width / newValue) / 2), offSetY - ((height / newValue) / 2), width / newValue, height / newValue));
-        });
-        //imageView.setCenter(image);
-        //imageView.setTop(Hscroll);
-        //imageView.setRight(Vscroll);
-        zoomLvl.valueProperty().addListener(e -> {
-            zoomlvl = zoomLvl.getValue();
-            double newValue = (double) ((int) (zoomlvl * 10)) / 10;
-            if (offSetX < (width / newValue) / 2) {
-                offSetX = (width / newValue) / 2;
-            }
-            if (offSetX > width - ((width / newValue) / 2)) {
-                offSetX = width - ((width / newValue) / 2);
-            }
-            if (offSetY < (height / newValue) / 2) {
-                offSetY = (height / newValue) / 2;
-            }
-            if (offSetY > height - ((height / newValue) / 2)) {
-                offSetY = height - ((height / newValue) / 2);
-            }
-            Hscroll.setValue(offSetX);
-            Vscroll.setValue(height - offSetY);
-            image.setViewport(new Rectangle2D(offSetX - ((width / newValue) / 2), offSetY - ((height / newValue) / 2), width / newValue, height / newValue));
-        });
-        imageView.setCursor(Cursor.OPEN_HAND);
-        image.setOnMousePressed(e -> {
-            initx = e.getSceneX();
-            inity = e.getSceneY();
-            imageView.setCursor(Cursor.CLOSED_HAND);
-        });
-        image.setOnMouseReleased(e -> {
-            imageView.setCursor(Cursor.OPEN_HAND);
-        });
-        image.setOnMouseDragged(e -> {
-            Hscroll.setValue(Hscroll.getValue() + (initx - e.getSceneX()));
-            Vscroll.setValue(Vscroll.getValue() - (inity - e.getSceneY()));
-            initx = e.getSceneX();
-            inity = e.getSceneY();
-        });
-
     }
 }
