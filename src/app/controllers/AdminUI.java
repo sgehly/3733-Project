576ee2d58ed7ef96147dc883e6f130ab1ed082b9
Sam Gehly <sam@gehly.net>
2019-04-01 18:56:33 -0400

    Dots!

--- src/app/controllers/AdminUI.java
+++ src/app/controllers/AdminUI.java
@@ -3,6 +3,8 @@ package app.controllers;
 import app.Main;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Orientation;
@@ -13,6 +15,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -26,12 +29,24 @@ import java.sql.*;
 
 public class AdminUI {
 
+    public class MapPoint {
+        double x;
+        double y;
+
+        public MapPoint(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     static double initx;
     static double inity;
     static int height;
     static int width;
     public static String path;
     static double offSetX,offSetY,zoomlvl;
+
+    Rectangle2D primaryScreenBounds;
 
     String dbPath = "jdbc:derby:myDB";
 
@@ -48,27 +63,101 @@ public class AdminUI {
     private Pane buttonContainer;
 
     @FXML
+    private TextField nodeIdTextBox;
+
+    @FXML
+    private TextField xCoordTextBox;
+
+    @FXML
+    private TextField yCoordTextBox;
+
+    @FXML
+    private TextField floorTextBox;
+
+    @FXML
+    private TextField buildingTextBox;
+
+    @FXML
+    private TextField typeTextBox;
+
+    @FXML
+    private TextField longNameTextBox;
+
+    @FXML
+    private TextField shortNameTextBox;
+
+    @FXML
     private void navigateToHome() throws Exception{
         Parent pane = FXMLLoader.load(Main.getFXMLURL("home"));
         Scene scene = new Scene(pane);
         Main.getStage().setScene(scene);
     }
 
-    private ObservableList<DisplayTable> getEntryObjects(ResultSet rs) throws SQLException {
+    private MapPoint scalePoints(int pointX, int pointY){
+        double rawWidth = 5000;
+        double rawHeight = 3400;
+
+        double scaledWidth = imageView.getBoundsInParent().getWidth();
+        double scaledHeight = imageView.getBoundsInParent().getHeight()-50;
+
+        System.out.println(scaledHeight+" - "+scaledWidth);
+        double scaledX = (pointX*scaledWidth)/rawWidth;
+        double scaledY = (pointY*scaledHeight)/rawHeight;
+        System.out.println(scaledX+" - "+scaledY);
+        return new MapPoint(scaledX, scaledY);
+
+    }
+
+    private void setValues(ActionEvent value){
+        try {
+            String nodeId = ((Button)value.getSource()).getId();
+            System.out.println(nodeId);
+            //TODO: Can someone on database make this so SQL Injection can't happen
+            String query = "SELECT * FROM FLOOR1 WHERE NODEID = ?";
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            Connection conn = DriverManager.getConnection(dbPath);
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, nodeId);
+            ResultSet rs = stmt.executeQuery();
+
+            if(rs.next()){
+                nodeIdTextBox.setText(rs.getString("nodeID"));
+                xCoordTextBox.setText(String.valueOf(rs.getInt("xCoord")));
+                yCoordTextBox.setText(String.valueOf(rs.getInt("yCoord")));
+                floorTextBox.setText(rs.getString("floor"));
+                buildingTextBox.setText(rs.getString("building"));
+                typeTextBox.setText(rs.getString("type"));
+                longNameTextBox.setText(rs.getString("longName"));
+                shortNameTextBox.setText(rs.getString("shortName"));
+            }
+
+        }
+        catch (Exception e) {
+            System.out.println("Error while trying to fetch all records");
+            e.printStackTrace();
+        }
+    }
+
+    private ObservableList<DisplayTable> getEntryObjects(ResultSet rs) throws Exception, SQLException {
         ObservableList<DisplayTable> entList = FXCollections.observableArrayList();
         try {
             while (rs.next()) {
                 Button newButton = new Button();
-                double size = 20;
+                double size = 5;
                 newButton.setMinWidth(size);
                 newButton.setMaxWidth(size);
                 newButton.setMinHeight(size);
                 newButton.setPrefHeight(size);
                 newButton.setPrefWidth(size);
                 newButton.setMaxHeight(size);
-                newButton.setLayoutX(rs.getInt("xcoord")-(size/2));
-                newButton.setLayoutY(rs.getInt("ycoord")-(size/2));
-                System.out.println("New Button! ("+String.valueOf(rs.getInt("xcoord")-(size/2))+","+String.valueOf(rs.getInt("ycoord")-(size/2))+")");
+                newButton.setId(rs.getString("nodeId"));
+                newButton.setOnAction(this::setValues);
+
+                MapPoint generated = scalePoints(rs.getInt("xcoord"), rs.getInt("ycoord"));
+                newButton.setLayoutX(generated.x-(size/2));
+                newButton.setLayoutY(generated.y-(size/2));
+                newButton.setStyle("-fx-background-color: blue");
+                System.out.println("New Button! ("+String.valueOf(rs.getInt("xcoord")-(size/2))+","+String.valueOf(rs.getInt("ycoord")-(size/2))+") -- ("+String.valueOf(primaryScreenBounds.getWidth())+","+String.valueOf(primaryScreenBounds.getHeight()-200)+") => ("+generated.x+","+generated.y+")");
                 buttonContainer.getChildren().add(newButton);
             }
             return entList;
@@ -80,7 +169,7 @@ public class AdminUI {
     }
 
 
-    public ObservableList<DisplayTable> getAllRecords() throws ClassNotFoundException, SQLException {
+    public ObservableList<DisplayTable> getAllRecords() throws ClassNotFoundException, SQLException, Exception {
         String query = "SELECT * FROM FLOOR1";
         try {
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
@@ -101,7 +190,7 @@ public class AdminUI {
 
         //Adapted from: https://stackoverflow.com/questions/48687994/zooming-an-image-in-imageview-javafx
         //------------------------------------------------------------------------------------------------
-        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        primaryScreenBounds = Screen.getPrimary().getVisualBounds();
         Image source = null;
         try {
             String path = getClass().getResource("/resources/maps/01_thefirstfloor.png").toString().replace("file:","");
@@ -124,11 +213,11 @@ public class AdminUI {
             width = 500;
         }
         image.setImage(source);
-        System.out.println(imageView.getBoundsInParent().getWidth());
         image.setFitWidth(primaryScreenBounds.getWidth());
         image.setFitHeight(primaryScreenBounds.getHeight()-200);
 
-        buttonContainer.setStyle("-fx-background-color: rgba(255,0,0,.4)");
+        buttonContainer.setPrefWidth(image.getFitWidth());
+        buttonContainer.setPrefHeight(image.getFitHeight());
         getAllRecords();
     }
 
