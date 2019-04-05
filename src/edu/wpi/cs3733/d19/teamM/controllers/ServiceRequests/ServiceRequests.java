2c31198ef64dfb29f21c36406727239fd938b496
joseph cybul <jcybul@wpi.edu>
2019-04-05 17:25:12 -0400

    language request

--- src/edu/wpi/cs3733/d19/teamM/controllers/ServiceRequests/ServiceRequests.java
+++ src/edu/wpi/cs3733/d19/teamM/controllers/ServiceRequests/ServiceRequests.java
@@ -6,13 +6,15 @@ import edu.wpi.cs3733.d19.teamM.utilities.DatabaseUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import java.sql.*;
 import java.awt.event.MouseEvent;
 import java.lang.String;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.Random;
+
 
 import java.io.IOException;
 
@@ -32,6 +34,23 @@ public class ServiceRequests {
     //initializing the timestamp
     long time = date.getTime();
     Timestamp ts = new Timestamp(time);
+
+    ObservableList<String> languages = FXCollections.observableArrayList("Acholi",
+            "Afrikaans", "Akan", "Albanian","Amharic","Arabic", "Ashante", "Asl", "Assyrian", "Azerbaijani", "Azeri", "Bajuni", "Basque",
+            "Behdini", "Belorussian", "Bengali", "Berber", "Bosnian", "Bravanese", "Bulgarian","Burmese", "Cakchiquel", "Cambodian", "Cantonese","Catalan",
+            "Chaldean", "Chamorro", "Chao-chow", "Chavacano", "Chin", "Chuukese", "Cree", "Croatian", "Czech", "Dakota", "Danish", "Dari", "Dinka",
+            "Diula", "Dutch", "Edo", "English", "Estonian", "Ewe", "Fante", "Farsi", "Fijian Hindi", "Finnish", "Flemish", "French", "French Canadian", "Fukienese",
+            "Fula", "Fulani", "Fuzhou", "Ga", "Gaddang", "Gaelic", "Gaelic-irish", "Gaelic-scottish", "Georgian", "German", "Gorani", "Greek", "Gujarati",
+            "Haitian Creole", "Hakka", "Hakka-chinese", "Hausa", "Hebrew", "Hindi", "Hmong", "Hungarian", "Ibanag", "Ibo", "Icelandic", "Igbo", "Ilocano",
+            "Indonesian", "Inuktitut", "Italian", "Jakartanese", "Japanese", "Javanese", "Kanjobal", "Karen", "Karenni", "Kashmiri", "Kazakh", "Kikuyu",
+            "Kinyarwanda", "Kirundi", "Korean", "Kosovan", "Kotokoli", "Krio", "Kurdish", "Kurmanji", "Kyrgyz", "Lakota", "Laotian",
+            "Latvian", "Lingala", "Lithuanian", "Luganda", "Luo", "Maay", "Macedonian", "Malay", "Malayalam", "Maltese", "Mandarin", "Mandingo", "Mandinka",
+            "Marathi", "Marshallese", "Mien", "Mina", "Mirpuri", "Moldavan", "Mongolian", "Montenegrin", "Navajo", "Neapolitan", "Nepali", "Nigerian Pidgin",
+            "Norwegian", "Oromo", "Pahari", "Papago", "Papiamento", "Pashto", "Patois", "Pidgin English", "Polish", "Portug.creole", "Portuguese", "Pothwari",
+            "Pulaar", "Punjabi", "Putian", "Quichua", "Romanian", "Russian", "Samoan", "Serbian", "Shanghainese", "Shona", "Sichuan", "Sicilian", "Sinhalese", "Slovak",
+            "Somali", "Sorani", "Spanish", "Sudanese Arabic", "Sundanese", "Susu", "Swahili", "Swedish", "Sylhetti", "Tagalog", "Taiwanese", "Tajik", "Tamil",
+            "Telugu", "Thai", "Tibetan", "Tigre", "Tigrinya", "Toishanese", "Tongan", "Toucouleur", "Trique", "Tshiluba", "Turkish", "Twi", "Ukrainian", "Urdu", "Uyghur", "Uzbek",
+            "Vietnamese", "Visayan", "Welsh", "Wolof", "Yiddish", "Yoruba", "Yupik");
 
     @FXML
     private Accordion accordion;
@@ -58,7 +77,7 @@ public class ServiceRequests {
     private TextArea languageRoomNumber;
 
     @FXML
-    private ChoiceBox<?> languageSelection;
+    private ChoiceBox<String> languageSelection;
 
     @FXML
     private Button languageRequestButton;
@@ -154,14 +173,47 @@ public class ServiceRequests {
         }
 
 
+
     /**
      * This method allows the user to create a language request using the button
      * @param e: The action that is associated with making the language request
      */
 
     @FXML
-    void makeLanguageRequest(ActionEvent e){
+    void makeLanguageRequest(ActionEvent e) throws Exception{
+        System.out.println("----Test Connection----");
+
+        try {
+
+            Connection conn = new DatabaseUtils().getConnection();
+            String query = "insert into  APP.REQUESTINPROGRESS  (REQUESTID , ROOM , NOTE , DATE,TYPE ) values (?,?,?,?,?)";
+            //
+            //PreparedStatement stmt=conn.prepareStatement("insert into  REQUESTINPROGRESS set REQUESTID = ? , ROOM = ?, NOTE = ?, DATE = ?, STATUS = ?");
+            //PreparedStatement stmt = conn.prepareStatement("SELECT * from REQUESTINPROGRESS");
+            PreparedStatement stmt = conn.prepareStatement(query);
+
+            stmt.setInt(1, (RandIDgenerator()));
+            stmt.setString(2, (languageRoomNumber.getText()));
+            stmt.setString(3, "The language is" + languageSelection.getValue() + "/n" + languageNotes.getText());
+            stmt.setTimestamp(4, ts);
+            stmt.setString(5,"Language");
+            //stmt.setBoolean(5, false);
+            System.out.println("in table ");
+
+            stmt.executeUpdate();
+            stmt.close();
+
+            this.goToRequestList();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
+
+
+
+
 
 
     /**
@@ -169,6 +221,10 @@ public class ServiceRequests {
      */
     @FXML// This method is called by the FXMLLoader when initialization is complete
     void initialize(){
+
+           accordion.setExpandedPane(sanitation);
+           languageSelection.setItems(languages);
+
             accordion.setExpandedPane(sanitation);
             //general asserts
             assert logoutButton != null : "fx:id=\"logoutButton\" was not injected: check your FXML file 'serviceRequests.fxml'.";
