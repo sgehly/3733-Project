79c3096d078d9db49033524a972ee992dd3c15da
Bridget <bmclean@wpi.edu>
2019-04-11 22:16:08 -0400

    Part 2? Updated UI on Int, Ext, Prescription, IT, and lab test

--- src/edu/wpi/cs3733/d19/teamM/controllers/ServiceRequests/ITRequests.java
+++ src/edu/wpi/cs3733/d19/teamM/controllers/ServiceRequests/ITRequests.java
@@ -5,8 +5,12 @@ import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733.d19.teamM.Main;
 import edu.wpi.cs3733.d19.teamM.User.User;
 import edu.wpi.cs3733.d19.teamM.utilities.Clock;
+import edu.wpi.cs3733.d19.teamM.utilities.DatabaseUtils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import javafx.scene.text.Text;
@@ -15,6 +19,9 @@ import org.controlsfx.control.textfield.TextFields;
 import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
 public class ITRequests implements Initializable {
@@ -26,7 +33,7 @@ public class ITRequests implements Initializable {
     private Text userText;
 
     @FXML
-    private JFXTextField typeOfEquipment;
+    private JFXTextField type;
 
     //Text field for room location input
     @FXML
@@ -41,6 +48,9 @@ public class ITRequests implements Initializable {
 
     @FXML
     private Button submitReuqest;
+
+    @FXML
+    private ListView listEmployees;
 
     @FXML
     private javafx.scene.control.Label lblClock;
@@ -67,7 +77,7 @@ public class ITRequests implements Initializable {
                 errorMessage.setText("You didn't answer all the required fields.");
                 throw e;
             }
-            new ServiceRequests().makeRequest("it", room.getText(), typeOfEquipment.getText(), notes.getText(), urgent.isSelected());
+            new ServiceRequests().makeRequest("it", room.getText(), type.getText(), notes.getText(), urgent.isSelected());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -75,7 +85,7 @@ public class ITRequests implements Initializable {
     }
 
     private boolean areFieldsEmpty() {
-        return room.getText().isEmpty() || typeOfEquipment.getText().isEmpty();
+        return room.getText().isEmpty() || type.getText().isEmpty();
     }
 
     @FXML
@@ -85,6 +95,27 @@ public class ITRequests implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        ObservableList<String> list = FXCollections.observableArrayList();
+
+        String query = "select * FROM users Where ACCOUNTINT = ?";
+        Connection conn = new DatabaseUtils().getConnection();
+        try{
+            PreparedStatement s = conn.prepareStatement(query);
+            s.setInt(1, 1); //CHANGE THIS FOR IT EMPLOYEES ???
+            ResultSet rs = s.executeQuery();
+            while(rs.next()){
+                list.add(rs.getString(1));
+                System.out.println(rs.getString(1));
+            }
+            for(String s1 : list){
+                listEmployees.getItems().add(s1);
+            }
+            conn.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
 
         Clock clock = new Clock(lblClock, lblDate);
         //userText.setText(User.getUsername());
--- src/edu/wpi/cs3733/d19/teamM/controllers/ServiceRequests/PrescriptionRequest.java
+++ src/edu/wpi/cs3733/d19/teamM/controllers/ServiceRequests/PrescriptionRequest.java
@@ -4,18 +4,28 @@ import com.jfoenix.controls.JFXCheckBox;
 import edu.wpi.cs3733.d19.teamM.Main;
 import edu.wpi.cs3733.d19.teamM.User.User;
 import edu.wpi.cs3733.d19.teamM.utilities.Clock;
+import edu.wpi.cs3733.d19.teamM.utilities.DatabaseUtils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 import java.awt.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class PrescriptionRequest {
 
     @FXML
     private Text userText;
+
+    @FXML
+    private ListView listEmployees;
 
     @FXML
     Label lblClock;
@@ -83,6 +93,29 @@ public class PrescriptionRequest {
 
     @FXML
     private void initialize(){
+
+        ObservableList<String> list = FXCollections.observableArrayList();
+
+        String query = "select * FROM users Where ACCOUNTINT = ?";
+        Connection conn = new DatabaseUtils().getConnection();
+        try{
+            PreparedStatement s = conn.prepareStatement(query);
+            s.setInt(1, 1);
+            ResultSet rs = s.executeQuery();
+            while(rs.next()){
+                list.add(rs.getString(1));
+                System.out.println(rs.getString(1));
+            }
+            for(String s1 : list){
+                listEmployees.getItems().add(s1);
+            }
+            conn.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
         new Clock(lblClock, lblDate);
         //userText.setText(User.getUsername());
         userText.setText("");
