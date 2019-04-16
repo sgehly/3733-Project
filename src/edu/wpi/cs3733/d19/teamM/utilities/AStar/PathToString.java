0e1ad74d669bb3bbec556b51159cc573f0764b9d
Jack <jfgerulskis@wpi.edu>
2019-04-16 16:32:57 -0400

    absolute directions for robot

--- src/edu/wpi/cs3733/d19/teamM/utilities/AStar/PathToString.java
+++ src/edu/wpi/cs3733/d19/teamM/utilities/AStar/PathToString.java
@@ -1,7 +1,10 @@
 package edu.wpi.cs3733.d19.teamM.utilities.AStar;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class PathToString {
 
@@ -68,6 +71,32 @@ public class PathToString {
         }
 
         return path.toString();
+    }
+
+    public static List<List<Double>> pathToInstructions(Path paths){
+        List<List<Double>> instructions = new ArrayList<>();
+        double angle, oldAngle, distance = 0;
+        int oldX, oldY = 0;
+        if (paths.getPath().size() < 3) return null;
+
+        for (Path p : paths.getFloorPaths()) {
+            if (p.getPath().size() > 1) {
+                oldAngle = calcAngle(p.getPath().get(0).getX(), p.getPath().get(0).getY(), p.getPath().get(1).getX(), p.getPath().get(1).getY(), distance);
+                oldX = p.getPath().get(1).getX();
+                oldY = p.getPath().get(1).getY();
+            }
+            for (int i = 1; i < p.getPath().size() - 2; i++) {
+                Node n = p.getPath().get(i);
+                Node next = p.getPath().get(i + 1);
+                angle = calcAngle(oldX, oldY, next.getX(), next.getY(), distance);
+                distance = (getDistance(oldX, oldY, next.getX(), next.getY()) * 0.34);
+                instructions.add(Arrays.asList(angle, distance));
+                oldX = next.getX();
+                oldY = next.getY();
+                oldAngle = angle;
+            }
+        }
+        return instructions;
     }
 
     private static double getDistance(int x, int y, int x1, int y1){
